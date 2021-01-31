99fe95b0c3e5d5d21a68addf12d3d00fd356f3a7
Matias <matias.lazos@gmail.com>
2021-01-31 18:47:21 -0300

    add mainnet contract

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = "TAuc2FhP4CipUo7atuHH9C7XvTp6vBgqym";
+export const CONTRACT_ADDRESS = "TFXEBkmhryZwJRUaAHe6jk3fpLdDiDhNVA";
 export const TRX_UNIT = 1000000;
 export const MAX_DECIMAL = 2;
 export const LOG_COLOR = 'background: #222; color: #bada55';
